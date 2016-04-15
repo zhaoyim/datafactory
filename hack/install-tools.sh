7c8ed8a725ee5a5c82bd0383f4e09c0009661507
zonesan <san@zoobook.org>
2016-04-15 19:31:43 +0800

    Travis can't install vet anymore.

--- hack/install-tools.sh
+++ hack/install-tools.sh
@@ -9,23 +9,6 @@ source "${OS_ROOT}/hack/common.sh"
 GO_VERSION=($(go version))
 echo "Detected go version: $(go version)"
 
-echo "get go tools"
-echo "get godep"
 go get github.com/tools/godep
-echo "get vet"
-go get golang.org/x/tools/cmd/vet
-echo "get cover"
-go get golang.org/x/tools/cmd/cover 
-echo "get tools finished."
-
-# Check out a stable commit for go vet in order to version lock it to something we can work with
-pushd $GOPATH/src/golang.org/x/tools >/dev/null 2>&1
-  git fetch
-  git checkout c262de870b618eed648983aa994b03bc04641c72 
-popd >/dev/null 2>&1
-
-# Re-install using this version of the tool
-go install golang.org/x/tools/cmd/vet
-
 
 ret=$?; ENDTIME=$(date +%s); echo "$0 took $(($ENDTIME - $STARTTIME)) seconds"; exit "$ret"
